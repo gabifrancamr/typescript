2bb58c5640d3cf4836cebecc476a516f00f30e19
Gabriela França <gabrielafranca2008@hotmail.com>
2023-12-19 13:43:46 -0300

    alteração dos comentários

--- 05-enums/enums.ts
+++ 05-enums/enums.ts
@@ -29,5 +29,5 @@ enum DiasDaSemana {
   let dia: DiasDaSemana = DiasDaSemana.Quarta;
   console.log(dia); // Saída: 3, pois a enumeração começa a contar do zero
 
-  let nomeDoDia: string = DiasDaSemana[3]; // Retorna 'Terca'
-let numeroDoDia: number = DiasDaSemana.Quarta; // Retorna 4
+  let nomeDoDia: string = DiasDaSemana[3]; // Retorna 'Quarta'
+let numeroDoDia: number = DiasDaSemana.Quarta; // Retorna 3
